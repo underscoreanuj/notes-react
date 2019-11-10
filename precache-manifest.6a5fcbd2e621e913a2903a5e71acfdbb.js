5cea1af2fb4ecd0ec7fcdc3e77c9fc3d9b164d2f
underscoreanuj <underscoreanuj@gmail.com>
2019-11-10 09:31:24 +0530

    Updates

---
+++ precache-manifest.6a5fcbd2e621e913a2903a5e71acfdbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13c9fdc14f04730e72d27319ff2cdff",
+    "url": "/notes-react/index.html"
+  },
+  {
+    "revision": "343db1b87fd8cb3f8898",
+    "url": "/notes-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "daf442e41cd97b5e704e",
+    "url": "/notes-react/static/js/2.57d79d16.chunk.js"
+  },
+  {
+    "revision": "343db1b87fd8cb3f8898",
+    "url": "/notes-react/static/js/main.128fa879.chunk.js"
+  },
+  {
+    "revision": "07c9753c9be31ed7c3c6",
+    "url": "/notes-react/static/js/runtime-main.2445a3cc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/notes-react/static/media/logo.25bf045c.svg"
+  }
+]);
